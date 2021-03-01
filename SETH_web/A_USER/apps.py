0513820f58afee7bec02b0c1a6fcfbc3f16af287
kevin <kevinagusto28@gmail.com>
2021-03-01 18:42:47 +0700

    Initiated web: A B C

---
+++ SETH_web/A_USER/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AUserConfig(AppConfig):
+    name = 'A_USER'
---
+++ SETH_web/C_USER/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CUserConfig(AppConfig):
+    name = 'C_USER'
