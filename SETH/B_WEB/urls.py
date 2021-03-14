b82c6bf98321cd36ebfd8df49cafc2f5110f3e3f
kevin <kevinagusto28@gmail.com>
2021-03-14 18:17:44 +0700

    A_WEB logout done, B_WEB login, and logout done. Face recognition authentication on progress

--- SETH/B_WEB/urls.py
+++ SETH/B_WEB/urls.py
@@ -2,16 +2,13 @@ from django.urls import path
 from . import views
 # from face_core import views as face_view
 from django.shortcuts import render
+from User.views import *
 
 app_name = "b_web"
 
 urlpatterns = [
-    # path('', views.HomePageView.as_view(), name='home'),
-    # path('History',views.history_view),
-    # path('results', views.SearchResultsView.as_view(),name='results'),
-    # path('profile',views.profile_view),
-    # path('registration',views.registration),
+    path('', views.history),
+    path('login', common_user_login, name='buser_login'),
     path('history', views.history),
     path('face_add', lambda r: render(r, "front2/dashboard.html")),
-    # path('face_add_src', face_view.add_face_src, name="b_face_add")
 ]
--- SETH/B_WEB/views.py
+++ SETH/B_WEB/views.py
@@ -2,13 +2,32 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 from SETH.models import *
+from django.contrib.auth.decorators import user_passes_test, login_required
+from django.contrib.auth.decorators import REDIRECT_FIELD_NAME
 
-class HomePageView(TemplateView):
-    template_name = 'home.html'
+def buser_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/b_web/login'):
+    '''
+    Decorator for views that checks that the logged in user is a student,
+    redirects to the log-in page if necessary.
+    '''
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.usertype==UserAuthentication.B_TYPE,
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(
+            function,
+        )
+    return actual_decorator
 
+@login_required
+@buser_required
 def registration(request):
     return render(request, 'registration.html')
 
+@login_required
+@buser_required
 def history(request):
     if request.method=="GET":
         return render(request, 'front2/searchCert.html', {"history": list(Certificate.objects.all().order_by("-date"))})
@@ -23,6 +42,5 @@ def history(request):
         return render(request, 'front2/searchCert.html', {"history": certs})
     else:
         print("Invalid method")
-#
-# def face(request):
-#     if request.m
+
+
