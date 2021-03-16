36c2ab5ec3994ae66d0a0bf5e0bbcca57abdd5f4
delkirawan <74372891+delkirawan@users.noreply.github.com>
2021-03-16 10:43:06 +0700

    Add files via upload

---
+++ sethcapp/lib/search_rs.dart
@@ -0,0 +1,306 @@
+import 'package:sethcapp/constant.dart';
+import 'package:sethcapp/widgets/my_header.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class search_rs extends StatefulWidget {
+  @override
+  _search_rsState createState() => _search_rsState();
+}
+
+class _search_rsState extends State<search_rs> {
+  final controller = ScrollController();
+  double offset = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
+  void onScroll() {
+    setState(() {
+      offset = (controller.hasClients) ? controller.offset : 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        controller: controller,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            MyHeader(
+              image: "assets/icons/Drcorona.svg",
+              textTop: "Where to",
+              textBottom: "Test?",
+              offset: offset,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0xFFE5E5E5),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Expanded(
+                    child: DropdownButton(
+                      isExpanded: true,
+                      underline: SizedBox(),
+                      icon: SvgPicture.asset("assets/icons/search.svg"),
+                      value: "Search hospital",
+                      items: [
+                        'Search hospital',
+                        'Yogyakarta',
+                        'DKI Jakarta',
+                        'Banten',
+                        'Listprovinsi'
+                      ].map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  /*
+                  Text(
+                    "Jenis Sertifikat",
+                    style: kTitleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                   */
+                  /*
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SymptomCard(
+                          image: "assets/images/headache.png",
+                          title: "PCR",
+                          isActive: true,
+                        ),
+                        SymptomCard(
+                          image: "assets/images/caugh.png",
+                          title: "Swab",
+                        ),
+                        SymptomCard(
+                          image: "assets/images/fever.png",
+                          title: "Rapid",
+                        ),
+                      ],
+                    ),
+                  ),
+                  */
+                  SizedBox(height: 20),
+                  Text("Result", style: kTitleTextstyle),
+                  PreventCard(
+                    text: "PCR / Rapid / Swab tests available" +
+                        "\n" +
+                        "1.8 kilometers from you",
+                    image: "assets/images/place.png",
+                    title: "RS Cilandak",
+                  ),
+                  SizedBox(height: 20),
+                  Text("Recommended hospitals", style: kTitleTextstyle),
+                  PreventCard(
+                    text: "PCR / Rapid / Swab tests available" +
+                        "\n" +
+                        "1.8 kilometers from you",
+                    image: "assets/images/place.png",
+                    title: "RS Fatmawati",
+                  ),
+                  PreventCard(
+                    text: "PCR " +
+                        "/ Swab tests available" +
+                        "\n" +
+                        "1.8 kilometers from you",
+                    image: "assets/images/place.png",
+                    title: "Puskesmas Pondok Indah",
+                  ),
+                  PreventCard(
+                    text:
+                        "PCR test available" + "\n" + "1.8 kilometers from you",
+                    image: "assets/images/place.png",
+                    title: "RSUD Pasar Minggu",
+                  ),
+                  Text("Maps View", style: kTitleTextstyle),
+                  SizedBox(height: 20),
+                  MapsCard(
+                    image: "",
+                  ),
+                  SizedBox(height: 80),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key key,
+    this.image,
+    this.title,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 120,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    blurRadius: 24,
+                    color: kShadowColor,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(image),
+            Positioned(
+              left: 70,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 17),
+                height: 110,
+                width: MediaQuery.of(context).size.width - 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: kTitleTextstyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        text,
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: kBodyTextColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) {
+                                //   return ();
+                              },
+                            ),
+                          );
+                        },
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: SvgPicture.asset("assets/icons/forward.svg"),
+                        )),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MapsCard extends StatelessWidget {
+  final String image;
+  const MapsCard({
+    Key key,
+    this.image,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 320,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    blurRadius: 24,
+                    color: kShadowColor,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(image),
+            Positioned(
+              left: 50,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                height: 90,
+                width: MediaQuery.of(context).size.width - 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
