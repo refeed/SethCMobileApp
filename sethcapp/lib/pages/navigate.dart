124f8a60340a20c3d5face3aec24847cefda0c27
kevin <kevinagusto28@gmail.com>
2021-03-28 09:33:45 +0700

    Dummy gmaps navigator

--- sethcapp/lib/pages/navigate.dart
+++ sethcapp/lib/pages/navigate.dart
@@ -6,14 +6,14 @@ THE PROJECT IS NOT DONE YET, SO IT'S ONLY PROTOTYPE
 
 */
 
-void navigateTo(double lat, double lng) async {
-   var uri = Uri.parse("https://www.google.com/maps?cid=15546286145103784455");
-   if (await canLaunch(uri.toString())) {
-      await launch(uri.toString());
-   } else {
-      throw 'Could not launch ${uri.toString()}';
-   }
-}
+// void navigateTo(double lat, double lng) async {
+//    var uri = Uri.parse("https://www.google.com/maps?cid=15546286145103784455");
+//    if (await canLaunch(uri.toString())) {
+//       await launch(uri.toString());
+//    } else {
+//       throw 'Could not launch ${uri.toString()}';
+//    }
+// }
 
 void navigateToBHC() async {
    var uri = Uri.parse("https://www.google.com/maps?cid=14064355031271158402");
